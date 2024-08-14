7f588701c3c538696544e89c29028c4b5680715b
Kyere <rnstkr@gmail.com>
2024-08-14 22:22:28 +0000

    controllers

---
+++ controllers/weatherController.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+const apiBaseURL = "http://api.openweathermap.org/data/2.5";
+const apiKey = process.env.API_KEY; // Loading the API key from environment variables
+
+// Fetching the recent weather
+exports.getCurrentWeather = async (req, res) => {
+  const { city } = req.query;
+  try {
+    const response = await axios.get(`${apiBaseURL}/weather`, {
+      params: {
+        q: city,
+        appid: apiKey, // API key in the request is included
+        units: 'metric'
+      }
+    });
+    res.json(response.data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// Fetching the weather forecast
+exports.getWeatherForecast = async (req, res) => {
+  const { city } = req.query;
+  try {
+    const response = await axios.get(`${apiBaseURL}/forecast`, {
+      params: {
+        q: city,
+        appid: apiKey, // API key in the request is included
+        units: 'metric'
+      }
+    });
+    res.json(response.data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
